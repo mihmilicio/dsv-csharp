b4feec0a8fc209b70fe8c097e8988441e9fcfbc6
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2021-08-11 22:09:18 -0300

    Ex6 - limpa código

--- ListaExercicio1/Views/SortVector.cs
+++ ListaExercicio1/Views/SortVector.cs
@@ -1,36 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 namespace ListaExercicio1.Views
 {
     public static class SortVector
     {
-        private static readonly Random Random = new Random();
-        static readonly int[] NumberList = new int[10];
+        private static readonly Random Random = new();
+        private static readonly int[] NumberList = new int[10];
         private const int MaxValue = 500;
-        
+
         public static void Exec()
         {
             // fill array randomly
-            for (int i = 0; i < NumberList.Length; i++)
+            for (var i = 0; i < NumberList.Length; i++)
             {
                 NumberList[i] = Random.Next(MaxValue);
             }
+
             DisplayArray(NumberList);
-            
+
             // TODO order it manually
-            
-            
+
+
             // order it programmatically 
             Array.Sort(NumberList);
             DisplayArray(NumberList);
         }
 
-        private static void DisplayArray(int[] array)
+        private static void DisplayArray(IEnumerable<int> array)
         {
             foreach (var item in array)
             {
                 Console.Write($"{item}   ");
             }
+
             Console.WriteLine("\n----");
         }
     }
