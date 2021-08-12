4b0d433bcf3354a2c6c4484df2ba6d47596c5bfe
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2021-08-11 22:10:42 -0300

    Ex1 e 2 - formatação

--- ListaExercicio1/Views/CurrencyExchange.cs
+++ ListaExercicio1/Views/CurrencyExchange.cs
@@ -4,11 +4,10 @@ namespace ListaExercicio1.Views
 {
     public static class CurrencyExchange
     {
-
         private const double DollarValue = 5.17;
         private const double EuroValue = 6.14;
         private const double PesoValue = 0.05;
-        
+
         public static void Exec()
         {
             double real;
@@ -25,22 +24,22 @@ namespace ListaExercicio1.Views
                 Console.WriteLine("Não foi possível processar o valor inserido. Tente novamente");
                 return;
             }
-            
-            Console.WriteLine($"Dólar: { FormatCurrency(CalcDollar(real)) }");
-            Console.WriteLine($"Euro: { FormatCurrency(CalcEuro(real)) }");
-            Console.WriteLine($"Pesos argentinos: { FormatCurrency(CalcPeso(real)) }");
+
+            Console.WriteLine($"Dólar: {FormatCurrency(CalcDollar(real))}");
+            Console.WriteLine($"Euro: {FormatCurrency(CalcEuro(real))}");
+            Console.WriteLine($"Pesos argentinos: {FormatCurrency(CalcPeso(real))}");
         }
 
         private static double CalcDollar(double real)
         {
             return real * DollarValue;
         }
-        
+
         private static double CalcEuro(double real)
         {
             return real * EuroValue;
         }
-        
+
         private static double CalcPeso(double real)
         {
             return real * PesoValue;
--- ListaExercicio1/Views/RectangleArea.cs
+++ ListaExercicio1/Views/RectangleArea.cs
@@ -4,24 +4,21 @@ namespace ListaExercicio1.Views
 {
     public static class RectangleArea
     {
-        
         public static void Exec()
         {
-            int height, width;
             Console.WriteLine("--------------------------------");
             Console.WriteLine("Calculadora de Área de Retângulo");
             Console.WriteLine("--------------------------------");
             Console.Write("Insira a altura: ");
-            height = Convert.ToInt32(Console.ReadLine());
+            var height = Convert.ToInt32(Console.ReadLine());
             Console.Write("Insira a largura: ");
-            width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Área: { Calc(height, width) }");
+            var width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Área: {Calc(height, width)}");
         }
 
         private static int Calc(int height, int width)
         {
             return height * width;
         }
-        
     }
 }
